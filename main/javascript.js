4477ba69290ae70563bfc1055481000b79bdc4e9
michael swigat <sherlockholmes.swigart92@gmail.com>
2022-03-15 20:03:12 -0500

    update1.4

--- main/javascript.js
+++ main/javascript.js
@@ -5,8 +5,13 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function (){
 //get random number between 0 - 3
-const randomNumber = 2;
-document.body.style.backgroundColor = color[randomNumber];
+const randomNumber = getRandomNumber();
+console.log(randomNumber);
+document.body.style.backgroundColor = colors[randomNumber];
 color.textContent = colors[randomNumber];
 console.log(document.body);
 });
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * colors.length);
+}
